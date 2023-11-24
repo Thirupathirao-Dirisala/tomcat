881b3e2743725e937fdc8b97900acf301db98d49
Thirupathirao-Dirisala <98281659+Thirupathirao-Dirisala@users.noreply.github.com>
2023-11-25 00:50:55 +0530

    Create LoginServlet.java

---
+++ src/main/webapp/LoginServlet.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Add your authentication logic here (e.g., check username and password against a database)
+
+        if ("your_username".equals(username) && "your_password".equals(password)) {
+            // Redirect to a success page if authentication is successful
+            response.sendRedirect("success.jsp");
+        } else {
+            // Set an error message attribute and forward back to the login page
+            request.setAttribute("errorMessage", "Invalid username or password");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+    }
+}
